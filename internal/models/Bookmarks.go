b6395a057c90636c7b2a04c7a185f78901a58abe
DevloperAmanSingh <amansinghneh211205@gmail.com>
2024-04-09 09:39:22 +0530

    Add new Bookmark model

---
+++ internal/models/Bookmarks.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Bookmark struct {
+	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID     primitive.ObjectID `json:"userId" bson:"userId"`
+	Username   string             `json:"username" bson:"username"`
+	CommentIDs []string           `json:"commentIds" bson:"commentIds"`
+	StoryIDs   []string           `json:"storyIds" bson:"storyIds"`
+	CreatedAt  time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
+}
